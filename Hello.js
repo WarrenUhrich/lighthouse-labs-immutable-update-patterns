2f6e91dc67b1c03cac571abbb14b2d0baf1175e3
Warren <hello@warren.codes>
2022-08-10 09:18:41 -0600

    Add component / props example.

---
+++ Hello.js
@@ -0,0 +1,6 @@
+const hello = (name) => {
+    return `Hello, ${name}!`;
+};
+
+console.log(hello('Archana'));
+console.log(hello('Mohammed'));
--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,6 +1,12 @@
+import Hello from './components/Hello';
+
 function App() {
   return (
-    <h1>Hello From the App Component</h1>
+    <>
+      <h1>Hello From the App Component</h1>
+      <Hello name="Archana" />
+      <Hello name="Mohammed" />
+    </>
   );
 }
 
