c5aa0ed3cbc4aedfa4a89355f2e13a4cb6e87c25
Warren <hello@warren.codes>
2022-08-10 10:09:27 -0600

    Add 'add topping' feature to Pizza component

---
+++ array-compare.js
@@ -0,0 +1,5 @@
+const myArray1 = [1, 2, 3, 4, 5];
+// const myArray2 = myArray1;
+const myArray2 = [1, 2, 3, 4, 5];
+
+console.log(myArray1 === myArray2);
--- demo-app/src/components/Pizza.jsx
+++ demo-app/src/components/Pizza.jsx
@@ -2,6 +2,16 @@ import { useState } from 'react';
 
 const Pizza = () => {
     const [toppings, setToppings] = useState(['cheese']);
+    const [newTopping, setNewTopping] = useState('');
+
+    const addTopping = () => {
+        // toppings.push(newTopping); // NEVER CHANGE STATE DIRECTLY!
+        // setToppings([...toppings, newTopping]);
+        setToppings((prev) => {
+            return [...prev, newTopping];
+        });
+        setNewTopping('');
+    };
 
     const toppingsListItems = toppings.map((topping, index) => {
         return <li key={index}>{topping}</li>;
@@ -11,8 +21,12 @@ const Pizza = () => {
         <>
             <h2>Create Your Own Pizza!</h2>
             <label htmlFor="new-topping">Please enter a pizza topping:</label>
-            <input id="new-topping" type="text" />
-            <button>Add Topping</button>
+            <input
+                id="new-topping"
+                type="text"
+                value={newTopping}
+                onChange={(event) => {setNewTopping(event.target.value);}} />
+            <button onClick={addTopping}>Add Topping</button>
             <h3>Your Toppings:</h3>
             <ul>{toppingsListItems}</ul>
         </>
